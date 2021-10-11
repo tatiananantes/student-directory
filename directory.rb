f8ae5c856623d2cdcfb421d8152c2d27594809a4
Tatiana Nantes <tatiananantes@gmail.com>
2021-10-11 18:46:16 +0100

    Refactor student array and variable count

--- directory.rb
+++ directory.rb
@@ -1,18 +1,23 @@
-student_count = 11
-# first we print the students list
-puts "The students of Villains Accademy"
-puts "-----------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
 
-# we print the student count
-puts "Overall, we have #{student_count} great students"
+# changing to students array
+students = [
+    "Dr. Hannibal Lecter",
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates"
+  ]
+# print them through indexes 
+puts "The students of Villains Academy"
+puts "-------------"
+students.each do |student|
+    puts student
+end
+
+puts "Overall, we have #{students.count} great students"
