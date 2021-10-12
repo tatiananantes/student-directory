be750829255dc2814422b9adbc31b5d80e6e983c
Tatiana Nantes <tatiananantes@gmail.com>
2021-10-12 14:15:35 +0100

    add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,17 +1,30 @@
-# Academy students
-# students = [
-#     {name: "Dr. Hannibal Lecter", cohort: :november},
-#     {name: "Darth Vader", cohort: :november},
-#     {name: "Nurse Ratched", cohort: :november},
-#     {name: "Michael Corleone", cohort: :november},
-#     {name: "Alex DeLarge", cohort: :november},
-#     {name: "The Wicked Witch of the West", cohort: :november},
-#     {name: "Terminator", cohort: :november},
-#     {name: "Freddy Krueger", cohort: :november},
-#     {name: "The Joker", cohort: :november},
-#     {name: "Joffrey Baratheon", cohort: :november},
-#     {name: "Norman Bates", cohort: :november}
-#   ]
+
+def interactive_menu
+  students = []
+  loop do
+# 1. print the menu and ask the user what to do
+puts "1. Input the students"
+puts "2. Show the students"
+puts "9. Exit"
+# 2. read the input and save it into a variable
+selection = gets.chomp
+# 3. do what the user has asked
+case selection
+  when "1"
+    students = input_students
+  when "2"
+    print_header
+    print(students)
+    print_footer(students)
+  when "9"
+    exit
+  else 
+    puts "Please try again."
+  end
+# 4. repeat from step 1  
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "to finish just hit return twice"
