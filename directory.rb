a642ea2cafb8e4127505c06b92ad3e953bb6c658
Tatiana Nantes <tatiananantes@gmail.com>
2021-10-13 12:33:18 +0100

    final solution

--- directory.rb
+++ directory.rb
@@ -1,24 +1,5 @@
 @students = []
 
-def input_students
-    puts "Please enter the names of the students"
-    puts "to finish just hit return twice"
-    name = gets.chomp
-    while !name.empty? do
-      @students << {name: name, cohort: :november}
-      puts "Now we have #{@students.count} students"
-      # get more names
-      name = gets.chomp
-    end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)  
-  end
-end
-
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
@@ -26,15 +7,16 @@ def print_menu
   puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
+  
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)  
+  end
 end
 
 def process (selection)
-    case selection
+  case selection
     when "1"
       input_students
     when "2"
@@ -47,7 +29,25 @@ def process (selection)
       exit
     else 
       puts "Please try again."
-    end  
+  end  
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "to finish just hit return twice"
+  name = STDIN.gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get more names
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -75,13 +75,26 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename,"r")
   file.readlines.each do |line|
-  name, cohort = line.chomp.split(",")
+  name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
 
+def try_load_students 
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? #get out the method if not given
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} does not exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
