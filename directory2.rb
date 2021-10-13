a59d6c6af5963738e07ce7510e8cadb939bfe3d7
Tatiana Nantes <tatiananantes@gmail.com>
2021-10-13 14:46:48 +0100

    student directory step8 exercise3 - prints only names with less than 12characters

--- directory2.rb
+++ directory2.rb
@@ -24,7 +24,7 @@ end
 
 def print(students)
   students.each do |student|
-    if student[:name].start_with? "J"
+    if student[:name].length <= 12
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
