d6dc6b5b00ace4bc00b2a0ff4d1156391dc11eb3
Tatiana Nantes <tatiananantes@gmail.com>
2021-10-13 14:07:12 +0100

    student directory step8 exercise1 - each with index

---
+++ directory2.rb
@@ -0,0 +1,39 @@
+# Academy students
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "to finish just hit return twice"
+#   create array for the names
+students = []
+# get the 1st name
+name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get more names
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each.with_index(1) do |student, index|
+    puts "#{index} #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+# calling the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
